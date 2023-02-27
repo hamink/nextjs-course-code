d97acc012bc5910fb964a8ddcea10c53c82defc9
hamink <hamin@atomrigs.io>
2023-02-26 21:07:45 -0700

    update firebase

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,22 +1,19 @@
-import { useHistory } from 'react-router-dom';
+import { useHistory } from "react-router-dom";
 
-import NewMeetupForm from '../components/meetups/NewMeetupForm';
+import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 function NewMeetupPage() {
   const history = useHistory();
 
   function addMeetupHandler(meetupData) {
-    fetch(
-      'https://react-getting-started-48dec-default-rtdb.firebaseio.com/meetups.json',
-      {
-        method: 'POST',
-        body: JSON.stringify(meetupData),
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      }
-    ).then(() => {
-      history.replace('/');
+    fetch("https://meet-up-7402c-default-rtdb.firebaseio.com/meetups.json", {
+      method: "POST",
+      body: JSON.stringify(meetupData),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }).then(() => {
+      history.replace("/");
     });
   }
 
